d089de7dcfa16031f4c5e23cf86280459f58e64a
NIMCOO <nimco77001@gmail.com>
2023-11-16 14:18:54 +0000

    Done Exercise

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Good Luck 💪🏾
+const  groups =[["Ahmed","Aisha","Amina"],["jaamac","khadiija","layla"],["mohamed","Ali"]]
+
+console. log(groups[0][0])
+console. log(groups[1][0])
+console. log(groups[2][0])
